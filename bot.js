b43a53db680c97b6f361dff40ac64688905495bb
sdaniels <daniels.scott.r@gmail.com>
2015-09-21 19:15:39 -0400

    updates to bot and park files for additional functionality

--- bot.js
+++ bot.js
@@ -8,15 +8,20 @@ stdin.resume();
 board.on("ready", function() {
     var motor1;
     var motor2;
-    var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V1;
+    var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V2;
 
     motor1 = new five.Motor(configs.M1);
-    motor2 = new five.Motor(configs.M4);
+    motor2 = new five.Motor(configs.M2);
 
     //// Create a new `ping` hardware instance.
-    var ping = new five.Ping(15);
-    ping.on("change", function() {
-        console.log("Object is " + this.in + " inches away");
+    //var ping = new five.Ping(15);
+    //ping.on("change", function() {
+    //    console.log("Object is " + this.in + " inches away");
+    //});
+
+    new five.Servo({
+        pin: 10,
+        range: [45, 135]
     });
 
     console.log("Use the cursor keys or ASWD to move your bot. Hit escape or the spacebar to stop.");
--- spark.js
+++ spark.js
@@ -3,6 +3,7 @@ var SPARK_DEVICE_ID = "55ff6e065075555329311787";
 
 var five = require("johnny-five");
 var Spark = require("spark-io");
+
 var board = new five.Board({
     io: new Spark({
         token: SPARK_TOKEN,
@@ -10,8 +11,56 @@ var board = new five.Board({
     })
 });
 
+var stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+
 board.on("ready", function() {
-    var led = new five.Led("D0");
-    console.log(led);
-    led.blink();
+    var motor1;
+    var motor2;
+    var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V1;
+
+    motor1 = new five.Motor(configs.M1);
+    motor2 = new five.Motor(configs.M3);
+
+    console.log("Use the cursor keys or ASWD to move your bot. Hit escape or the spacebar to stop.");
+
+    stdin.on("keypress", function(chunk, key) {
+
+        if (!key) return;
+
+        switch (key.name) {
+            case 'up':
+            case 'w':
+                // set the motor going forward full speed
+                motor1.forward(255);
+                motor2.forward(255);
+                break;
+
+            case 'down':
+            case 's':
+                motor1.reverse(175);
+                motor2.reverse(175);
+                break;
+
+            case 'left':
+            case 'a':
+                motor1.forward(255);
+                motor2.reverse(175);
+                break;
+
+            case 'right':
+            case 'd':
+                motor2.forward(255);
+                motor1.reverse(175);
+                break;
+
+            case 'space':
+            case 'escape':
+                motor1.stop();
+                motor2.stop();
+                break;
+        }
+    });
+
 });
