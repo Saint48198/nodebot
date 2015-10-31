1b0c8c7247f89b50df13a6a7a60689140456e959
sdaniels <daniels.scott.r@gmail.com>
2015-10-30 20:06:10 -0400

    code clean up and speed adj

--- bot.js
+++ bot.js
@@ -1,11 +1,15 @@
 var five = require("johnny-five");
-var board = new five.Board();
+var board = new five.Board({
+    port: "/dev/cu.3rc-DevB"
+});
 
 var stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
 
 board.on("ready", function() {
+    this.pinMode(13, this.MODES.OUTPUT);
+
     var motor1;
     var motor2;
     var configs = five.Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_3;
@@ -23,33 +27,33 @@ board.on("ready", function() {
     console.log("Use the cursor keys or ASWD to move your bot. Hit escape or the spacebar to stop.");
 
     stdin.on("keypress", function(chunk, key) {
-
+        console.log(key);
         if (!key) return;
 
         switch (key.name) {
             case 'up':
             case 'w':
                 // set the motor going forward full speed
-                motor1.forward(255);
-                motor2.forward(255);
+                motor1.forward(175);
+                motor2.forward(175);
                 break;
 
             case 'down':
             case 's':
-                motor1.reverse(175);
-                motor2.reverse(175);
+                motor1.reverse(125);
+                motor2.reverse(125);
                 break;
 
             case 'left':
             case 'a':
-                motor1.forward(255);
-                motor2.reverse(175);
+                motor1.forward(175);
+                motor2.reverse(125);
                 break;
 
             case 'right':
             case 'd':
-                motor2.forward(255);
-                motor1.reverse(175);
+                motor2.forward(175);
+                motor1.reverse(125);
                 break;
 
             case 'space':
